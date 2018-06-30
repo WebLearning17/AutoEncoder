664b5fc0e331507d4a64874180d0f350e4e35b7f
Yongyu Wu <wuyongyu@rd.netease.com>
2018-06-30 10:57:57 +0800

    自编码器

---
+++ convolutional_autoencoder.py
@@ -0,0 +1,87 @@
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.datasets import MNIST
+import os
+
+if not os.path.exists('./dc_img'):
+    os.mkdir('./dc_img')
+
+
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+
+num_epochs = 1
+batch_size = 128
+learning_rate = 1e-3
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = MNIST('./data', transform=img_transform, download=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+model = autoencoder()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+                             weight_decay=1e-5)
+
+printOutput=True
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = Variable(img)
+        # ===================forward=====================
+        output = model(img)
+        loss = criterion(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if printOutput:
+            print("img Size:",img.size())
+            print("output Size:",output.size())
+            printOutput=False
+    # ===================log========================
+
+    print("epoch output Size:",output.size())
+    print('epoch [{}/{}], loss:{:.4f}'
+          .format(epoch+1, num_epochs, loss.item()))
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)
+        save_image(pic, './dc_img/image_{}.png'.format(epoch))
